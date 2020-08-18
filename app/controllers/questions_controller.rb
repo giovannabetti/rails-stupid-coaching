2e6254151282f9ecb589f4e4b6f2dc89aa378499
Giovanna Betti <giovannabetti@gmail.com>
2020-08-17 22:30:33 -0300

    Solving

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+# app/controller/pages_controller.rb
+class QuestionsController < ApplicationController
+  def about
+  end
+
+  def contact
+  end
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# config/routes.rb
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'url', to: 'controller#action'
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+
+  # get 'ask',    to: 'questions#ask',    as: :ask
+  # get 'answer', to: 'questions#answer', as: :answer
 end
