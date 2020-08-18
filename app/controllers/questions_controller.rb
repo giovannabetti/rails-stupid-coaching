ffc0edffb53997065708002fabe6b04992031952
Giovanna Betti <giovannabetti@gmail.com>
2020-08-17 23:35:54 -0300

    Trying to solve exercise problem

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,18 @@
 # app/controller/pages_controller.rb
 class QuestionsController < ApplicationController
-  def about
+  def ask
   end
 
-  def contact
-  end
-
-  def home
+  def answer
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = 'Great!'
+    elsif @question.ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
